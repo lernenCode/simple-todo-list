f885ca5b3419f4c15a83e9c6bdce4af90759451b
Luiz <96671069+lernenCode@users.noreply.github.com>
2023-01-19 00:06:03 -0300

    Update script.js
    melhoria do código e correção de bug ao remover a tarefa.

--- script.js
+++ script.js
@@ -6,30 +6,26 @@ const mainDiv = document.querySelector(".main");
 
 main();
 
-function main() 
-{
+function main() {
   // Adiciona evento de carregar tarefas quando a página é carregada
   window.addEventListener("load", carregarTarefas);
   // Adiciona evento de adicionar tarefa quando o botão Adicionar é clicado
   adicionar.addEventListener("click", botaoAdicionar);
 
-  function botaoAdicionar(evento) 
-  {
-    if (titulo.value !== "") 
-    {
-        let tarefa = 
-        {
-          id: nextId++,
-          titulo: titulo.value,
-        }
-        
-        tarefas.push(tarefa);
-        criarElementos(tarefa.id, tarefa.titulo);
-        titulo.value = "";
-        localStorage.setItem("tarefas", JSON.stringify(tarefas));
+  function botaoAdicionar(evento) {
+    if (titulo.value !== "") {
+      let tarefa = {
+        id: nextId++,
+        titulo: titulo.value,
+      };
+
+      tarefas.push(tarefa);
+      criarElementos(tarefa.id, tarefa.titulo);
+      titulo.value = "";
+      localStorage.setItem("tarefas", JSON.stringify(tarefas));
     }
   }
-
+  
   function criarElementos(id, titulo) {
     var novaDiv = document.createElement("div");
     var novoTitulo = document.createElement("h6");
@@ -42,38 +38,32 @@ function main()
     mainDiv.appendChild(novaDiv);
     // Adicionar classes do Boostrap e CSS
     novoTitulo.setAttribute("class", "d-inline-block col-10 deco-title");
-    novoBotao.setAttribute("class", "d-inline-block float-right button-check col-2 p-2 mx-auto");
+    novoBotao.setAttribute(
+      "class",
+      "d-inline-block float-right button-check col-2 p-2 mx-auto"
+    );
     novaDiv.setAttribute("class", "glassLiquid mt-3 d-flex");
     //
     novoTitulo.innerHTML = titulo;
     // Adiciona evento de excluir tarefa quando o botão é clicado
     novoBotao.addEventListener("click", function (event) {
-      deleteDivAnim(event);
       deletarTarefa(this);
     });
-    function deleteDivAnim(event) {
-      const div = event.target.parentNode.parentNode;
-      div.classList.add("deleting");
-      setTimeout(() => {
-        div.remove();
-      }, 500);
-    }
-  }  
-
-  function excluir(tarefaDeletada) 
-  {
-    tarefaDeletada.parentNode.remove();
   }
 
-  function deletarTarefa(tarefaDeletada) 
-  {
+  function deletarTarefa(tarefaDeletada) {
     // Recupera o array de tarefas salvas no Local Storage
     let tarefasSalvas = JSON.parse(localStorage.getItem("tarefas"));
     // Encontra o id da tarefaDeletada no array
     let id = tarefaDeletada.getAttribute("data-id");
+    tarefaDeletada.parentNode.classList.add("deleting");
+    setTimeout(() => {
+      tarefaDeletada.parentNode.remove();
+    }, 500);
 
-    let indexSalvo = tarefasSalvas.findIndex(function (tarefa) 
-    {return tarefa.id === id;});
+    let indexSalvo = tarefasSalvas.findIndex(function (tarefa) {
+      return tarefa.id == id;
+    });
     // Remove a tarefa do array
     tarefasSalvas.splice(indexSalvo, 1);
     // Atualiza o Local Storage com o novo array
@@ -81,14 +71,13 @@ function main()
     tarefas = tarefasSalvas;
   }
 
-  function carregarTarefas() 
-  {
+  function carregarTarefas() {
     let tarefasSalvas = JSON.parse(localStorage.getItem("tarefas"));
-
-    if (tarefasSalvas) 
-    {
-      tarefasSalvas.forEach(function (tarefa) 
-      {criarElementos(tarefa.id, tarefa.titulo);});
+    if (tarefasSalvas) {
+      for (let i = 0; i < tarefasSalvas.length; i++) {
+        let tarefa = tarefasSalvas[i];
+        criarElementos(tarefa.id, tarefa.titulo);
+      }
     }
   }
 }
